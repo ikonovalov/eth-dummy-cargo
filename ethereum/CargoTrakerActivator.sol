c51cd20abbee2f8debd57852047e9f8578dd9d76
ikonovalov <konovalov84@gmail.com>
2017-01-18 23:18:49 +0300

    add imports

--- ethereum/CargoTrakerActivator.sol
+++ ethereum/CargoTrakerActivator.sol
@@ -1,16 +1,10 @@
 pragma solidity ^0.4.8;
-
-contract CargoTrackerService {
-  function createTrackingRequest(string trackNumber, string carrier, uint128 frequency, uint128 maxReps) external payable;
-}
-
-contract CargoTrackRequester {
-    function updateTrack(string information);
-}
+import "./CargoTrackerService.sol";
+import "./CargoTrackRequester.sol";
 
 contract CargoTrackerActivator is CargoTrackerService {
 
-    uint32 public constant version = 7;
+    uint public constant version = 8;
 
     address public owner;
 
@@ -26,6 +20,12 @@ contract CargoTrackerActivator is CargoTrackerService {
         }
     }
 
+    modifier onlyOperator() {
+        if (msg.sender != owner)
+            throw;
+        _;
+    }
+
     modifier costs(uint _amount) {
         if (msg.value < _amount)
             throw;
@@ -37,16 +37,18 @@ contract CargoTrackerActivator is CargoTrackerService {
 
     enum TrackingStatus {
         NOT_EXISTS, /* means not exist/registred */
-        UNDER_TRACKING,
-        BAD_TRACK_SETUP,
-        MAX_REPS_REACHED,
-        COMPLETED_SUCCESSFULLY
+        UNDER_TRACKING,         // on going
+        UNDER_SO_TRACKING,      // on going
+        BAD_TRACK_SETUP,        // terminated
+        MAX_RETRIES_REACHED,    // suspended
+        COMPLETED_SUCCESSFULLY, // terminated
+        TOO_HIGH_FREQ           // terminated
     }
 
     struct TrackingRequest {
         string carrier;
         uint128 frequency;
-        uint128 maxReps;
+        uint128 maxRetries;
         TrackingStatus status;
         address[] requestors;
     }
@@ -55,8 +57,9 @@ contract CargoTrackerActivator is CargoTrackerService {
     mapping(string => TrackingRequest) internal trackingRequests;
     uint256 public trackingRequestsCount = 0;
 
-    // frequency - minutes
-    function createTrackingRequest(string trackNumber, string carrier, uint128 frequency, uint128 maxReps) external payable costs(maxReps * 1 ether) {
+    // frequency - 1/day (2 means one hit in 12 hours)
+    // duration: freq = 2, maxRetr = 100 => 100/2 = 50 hours
+    function createTrackingRequest(string trackNumber, string carrier, uint128 frequency, uint128 maxRetries) external payable costs(maxRetries * 1 ether) {
         TrackingRequest req = trackingRequests[trackNumber];
         if (req.status == TrackingStatus.NOT_EXISTS) {
             address[]  memory reqs = new address[](1);
@@ -65,7 +68,7 @@ contract CargoTrackerActivator is CargoTrackerService {
                 {
                     carrier: carrier,
                     frequency: frequency,
-                    maxReps: maxReps,
+                    maxRetries: maxRetries,
                     status: TrackingStatus.UNDER_TRACKING,
                     requestors: reqs
                 }
@@ -74,7 +77,28 @@ contract CargoTrackerActivator is CargoTrackerService {
             trackingRequestsCount++;
         } else {
             req.requestors.push(msg.sender);
-            req.maxReps = req.maxReps + maxReps;
+            req.status = TrackingStatus.UNDER_SO_TRACKING;
+            // TODO: recalculate frequency. New duration can't be less when current.
+            //req.maxRetries = req.maxRetries + maxRetries;
+        }
+    }
+
+    function maxRetriesReached(string trackNumber) onlyOperator {
+        TrackingRequest req = trackingRequests[trackNumber];
+        if (req.status == TrackingStatus.UNDER_TRACKING) {
+            req.status = TrackingStatus.MAX_RETRIES_REACHED;
+            trackingRequestsCount--;
+            for (uint i = 0; i < req.requestors.length; i++) {
+                CargoTrackRequester(req.requestors[i]).maxRetriesReached(trackNumber);
+            }
+        }
+    }
+
+    // it can be done externally by oracle
+    function updateTrackForRequestor(string trackNumber, string info) onlyOperator {
+        TrackingRequest req = trackingRequests[trackNumber];
+        for (uint i = 0; i < req.requestors.length; i++) {
+            CargoTrackRequester(req.requestors[i]).updateTrack(info);
         }
     }
 
@@ -85,7 +109,7 @@ contract CargoTrackerActivator is CargoTrackerService {
     function getTrackRequest(string trackNumber) constant returns(
                     string carrier,
                     uint128 frequency,
-                    uint128 maxRetr,
+                    uint128 maxRetries,
                     uint status,
                     address[] requestors
     ) {
@@ -93,7 +117,7 @@ contract CargoTrackerActivator is CargoTrackerService {
         if (req.status != TrackingStatus.NOT_EXISTS) {
             carrier = req.carrier;
             frequency = req.frequency;
-            maxRetr = req.maxReps;
+            maxRetries = req.maxRetries;
             status = uint(req.status);
             requestors = req.requestors;
         }
