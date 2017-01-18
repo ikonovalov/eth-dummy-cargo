9791f4c6028491b8e2ef9c6c61337be48cf8eaca
ikonovalov <konovalov84@gmail.com>
2017-01-18 18:03:16 +0300

    V7 with getters

--- ethereum/CargoTrakerActivator.sol
+++ ethereum/CargoTrakerActivator.sol
@@ -1,15 +1,38 @@
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.8;
 
-contract CargoTrackerActivator {
+contract CargoTrackerService {
+  function createTrackingRequest(string trackNumber, string carrier, uint128 frequency, uint128 maxReps) external payable;
+}
 
-    uint32 public constant version = 1;
+contract CargoTrackRequester {
+    function updateTrack(string information);
+}
+
+contract CargoTrackerActivator is CargoTrackerService {
+
+    uint32 public constant version = 5;
+
+    address public owner;
+
+    uint public cash = 0;
+
+    function CargoTrackerActivator() {
+        owner = msg.sender;
+    }
+
+    function withdrawal() {
+        if (msg.sender == owner && msg.sender.send(cash)) {
+            cash = 0;
+        }
+    }
 
     modifier costs(uint _amount) {
         if (msg.value < _amount)
             throw;
         _;
-        if (msg.value > _amount)
-            msg.sender.send(msg.value - _amount);
+        if (msg.value > _amount && msg.sender.send(msg.value - _amount)) {
+                cash += _amount;
+        }
     }
 
     enum TrackingStatus {
@@ -21,25 +44,41 @@ contract CargoTrackerActivator {
     }
 
     struct TrackingRequest {
-        address requester;
         string carrier;
         uint128 frequency;
         uint128 maxReps;
         TrackingStatus status;
+        address[] requestors;
     }
 
     // trackNumber to request
     mapping(string => TrackingRequest) internal trackingRequests;
+    uint256 public trackingRequestsCount = 0;
 
-    function createTrackingService(string trackNumber, string carrier, uint128 frequency, uint128 maxReps) external payable costs(maxReps * 1 ether) {
+    function createTrackingRequest(string trackNumber, string carrier, uint128 frequency, uint128 maxReps) external payable costs(maxReps * 1 ether) {
         TrackingRequest req = trackingRequests[trackNumber];
         if (req.status == TrackingStatus.NOT_EXISTS) {
-            trackingRequests[trackNumber] = TrackingRequest(msg.sender, carrier, frequency, maxReps, TrackingStatus.UNDER_TRACKING);
+            address[]  memory reqs = new address[](1);
+            reqs[0] = msg.sender;
+            trackingRequests[trackNumber] = TrackingRequest(
+                {
+                    carrier: carrier,
+                    frequency: frequency,
+                    maxReps: maxReps,
+                    status: TrackingStatus.UNDER_TRACKING,
+                    requestors: reqs
+                }
+            );
+            trackingRequests[trackNumber].status = TrackingStatus.UNDER_TRACKING;
+            trackingRequestsCount++;
+        } else {
+            req.requestors.push(msg.sender);
+            req.maxReps = req.maxReps + maxReps;
         }
     }
 
-}
+    function getRequestorsFromTrack(string trackNumber) constant returns (address[]) {
+        return trackingRequests[trackNumber].requestors;
+    }
 
-contract CargoTrackRequester {
-    function updateTrack(string information);
 }
